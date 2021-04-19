de2f0e1755e07e85427590bd404b9b70ce8fd9fb
张治国 <wb-zzg884053@alibaba-inc.com>
2021-04-19 18:21:48 +0800

    contral dir

--- node/tree/1.bst.js
+++ node/tree/1.bst.js
@@ -1,6 +1,5 @@
 class Node {
   constructor(element, parent) {
-    super();
     this.element = element;
     this.parent = parent;
     this.left = null;
@@ -14,14 +13,113 @@ class Tree {
   add(element) {
     if (this.root === null) {
       this.root = new Node(element);
+      return;
     }
 
-    // 左右分开
+    let currentNode = this.root;
+    let parent;
+    let compare;
+    while (currentNode) {
+      compare = currentNode.element < element;
+      parent = currentNode;
+
+      if (compare) {
+        currentNode = currentNode.right;
+      } else {
+        currentNode = currentNode.left;
+      }
+    }
+
+    let node = new Node(element, parent);
+    if (compare) {
+      parent.right = node;
+    } else {
+      parent.left = node;
+    }
+  }
+
+  preorderTraversal() {
+    function traversal(node) {
+      if (node === null) return;
+      console.log(node.element);
+      traversal(node.left);
+      traversal(node.right);
+    }
+    traversal(this.root);
+  }
+  inorderTraversal() {
+    function traversal(node) {
+      if (node === null) return;
+      traversal(node.left);
+      console.log(node.element);
+      traversal(node.right);
+    }
+    traversal(this.root);
+  }
+  postorderTraversal() {
+    function traversal(node) {
+      if (node === null) return;
+      traversal(node.left);
+      traversal(node.right);
+      console.log(node.element);
+    }
+    traversal(this.root);
+  }
+  levelOrderTraversal(cb) {
+    let stack = [this.root];
+    let index = 0;
+    let currentNode;
+    while (currentNode = stack[index++]) {
+      cb(currentNode)
+      if (currentNode.left) {
+        stack.push(currentNode.left);
+      }
+
+      if (currentNode.right) {
+        stack.push(currentNode.right);
+      }
+    }
+
+  }
+
+  reverse(cb) {
+    let stack = [this.root];
+    let index = 0;
+    let currentNode;
+    while (currentNode = stack[index++]) {
+      let temp = currentNode.left;
+      currentNode.left = currentNode.right;
+      currentNode.right = temp;
+      if (currentNode.left) {
+        stack.push(currentNode.left);
+      }
+
+      if (currentNode.right) {
+        stack.push(currentNode.right);
+      }
+    }
+
   }
 }
 
 
 let tree = m = new Tree();
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 0].forEach(item => {
+[10, 8, 19, 34, 67, 32, 36, 98].forEach(item => {
   tree.add(item)
 })
+
+
+
+
+
+// tree.preorderTraversal();
+// tree.inorderTraversal();
+// tree.postorderTraversal();
+// tree.levelOrderTraversal((node) => {
+//   node.element *= 2
+// });
+tree.reverse();
+
+console.dir(tree, {
+  depth: 1000
+});
---
+++ node/tree/3.fs.js
@@ -0,0 +1,131 @@
+// const fs = require('fs').promises;
+const {
+  dir
+} = require('console');
+const fs = require('fs');
+const path = require('path');
+const {
+  existsSync
+} = require('fs');
+
+// fs.rmdir('a',function(err) {
+//   console.log(err);
+// })
+// fs.readdir('a', function (err,dirs) {
+//   console.log(dirs);
+// })
+// fs.stat('a', function (err,statObj) {
+//   console.log(statObj.isDirectory());
+// })
+
+// function rmdir(dir, cb) {
+//   fs.stat(dir, function (err, statObj) {
+//     if (statObj.isDirectory()) {
+//       // 文件夹
+//       fs.readdir(dir, function (err, dirs){
+//         dirs = dirs.map(item=> path.join(dir, item));
+//         //  把目录里面的拿出来， 1个删除完成后删除第二个
+//         let index = 0;
+//         function step() {
+//           // 子目录都删除，删除自己
+//           if (index === dirs.length) return fs.rmdir(dir, cb)
+//           rmdir(dirs[index++],step);
+//         }
+
+//         step();
+//       });
+//     } else {
+//       // 文件
+//       fs.unlink(dir,cb);
+//     }
+//   })
+// }
+function rmdir(dir, cb) {
+  fs.stat(dir, function (err, statObj) {
+    if (!statObj) return;
+    if (statObj.isDirectory()) {
+      // 文件夹
+      fs.readdir(dir, function (err, dirs) {
+        dirs = dirs.map(item => path.join(dir, item));
+
+        if (!dirs.length) {
+          return fs.rmdir(dir, cb)
+        }
+
+        let i = 0;
+        function done() {
+          if (++i === dirs.length) {
+            return fs.rmdir(dir, cb)
+          }
+        }
+
+        for (let i = 0; i < dirs.length; i++) {
+          rmdir(dirs[i], done);
+        }
+
+      });
+    } else {
+      // 文件
+      fs.unlink(dir, cb);
+    }
+  })
+}
+
+rmdir('a', function (err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('删除成功');
+  }
+})
+
+
+
+
+
+
+
+
+
+// async function mkdir(pathStr) {
+//   let pathList = pathStr.split('/');
+
+//   for(let i = 1; i <= pathList.length; i++) {
+//     let currentPath = pathList.slice(0, i).join('/');
+//     if (!existsSync(currentPath)) {
+//       await fs.mkdir(currentPath);
+//     }
+//   }
+// }
+
+// mkdir('a/b/c/d').then(() => {
+//   console.log('sucess');
+// }).catch(err=>{
+//   console.log(err);
+// })
+
+
+
+// function mkdir(pathStr, cb) {
+//   let pathList = pathStr.split('/');
+//   let index = 1;
+
+//   function make(err) {
+//     if (err) return cb(err);
+//     if (index === pathList.length + 1) return cb();
+//     let currentPath = pathList.slice(0, index++).join('/');
+//     fs.stat(currentPath, function (err) {
+//       if (err) {
+//         fs.mkdir(currentPath, make);
+//       } else {
+//         make();
+//       }
+//     })
+//   };
+//   make();
+// }
+
+// mkdir('a/b/c/d', function(err) {
+//   if (err) return console.log(err);
+//   console.log('sucess');
+// })
